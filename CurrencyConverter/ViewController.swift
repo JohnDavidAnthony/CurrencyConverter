805cd36fc3e702110e49908ee18abf6586d9f75f
John David <johndavid.anthony1@gmail.com>
2018-06-10 20:35:11 -0400

    Initial Commit

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by John David Anthony on 2018-06-10.
+//  Copyright © 2018 John David Anthony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
